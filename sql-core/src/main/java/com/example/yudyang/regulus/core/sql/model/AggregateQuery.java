bddca42d6221a80a4d7d15f5306053e05986ae1e
edmondYoung <xyang200710@gmail.com>
2021-05-10 16:32:07 +0800

    update

---
+++ sql-core/src/main/java/com/example/yudyang/regulus/core/sql/model/AggregateQuery.java
@@ -0,0 +1,2 @@
+package com.example.yudyang.regulus.core.sql.model;public class AggregateQuery {
+}
---
+++ sql-core/src/main/java/com/example/yudyang/regulus/core/sql/parser/aggregate/AggregationParser.java
@@ -0,0 +1,2 @@
+package com.example.yudyang.regulus.core.sql.parser.aggregate;public interface AggregationParser {
+}
