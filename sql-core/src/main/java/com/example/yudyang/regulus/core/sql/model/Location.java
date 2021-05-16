f9ff1cda19c7eccf55410291188187844ea5430c
edmondYoung <xyang200710@gmail.com>
2021-05-16 17:17:00 +0800

    up

--- sql-core/src/main/java/com/example/yudyang/regulus/core/sql/model/Location.java
+++ sql-core/src/main/java/com/example/yudyang/regulus/core/sql/model/Location.java
@@ -8,22 +8,23 @@ public class Location {
 
     /**
      * Create a new Location
+     *
      * @param sourceName script's name
-     * @param offset Character offset of script element
+     * @param offset     Character offset of script element
      */
-    public Location(String sourceName,int offset){
-        this.sourceName= Objects.requireNonNull(sourceName);
-        this.offset=offset;
+    public Location(String sourceName, int offset) {
+        this.sourceName = Objects.requireNonNull(sourceName);
+        this.offset = offset;
     }
 
-    public RuntimeException createError(RuntimeException exception){
-        StackTraceElement element=new StackTraceElement(WriterConstants.CLASS_NAME,"compile",sourceName,offset+1);
-        StackTraceElement[] oldStack=exception.getStackTrace();
-        StackTraceElement[] newStack=new StackTraceElement[oldStack.length+1];
-        System.arraycopy(oldStack,0,newStack,1,oldStack.length);
-        newStack[0]=element;
+    public RuntimeException createError(RuntimeException exception) {
+        StackTraceElement element = new StackTraceElement(WriterConstants.CLASS_NAME, "compile", sourceName, offset + 1);
+        StackTraceElement[] oldStack = exception.getStackTrace();
+        StackTraceElement[] newStack = new StackTraceElement[oldStack.length + 1];
+        System.arraycopy(oldStack, 0, newStack, 1, oldStack.length);
+        newStack[0] = element;
         exception.setStackTrace(newStack);
-        assert exception.getStackTrace().length==newStack.length:"non-writeable stacktrace for exception: "+exception.getClass();
+        assert exception.getStackTrace().length == newStack.length : "non-writeable stacktrace for exception: " + exception.getClass();
         return exception;
     }
 }
